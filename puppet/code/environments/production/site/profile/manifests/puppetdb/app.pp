3a4c01efb45266860b6dd5edc5fc4cabc389dc78
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-03-22 22:38:30 -0300

    Refatoracao para simplificar e flexibilizar a classe

--- puppet/code/environments/production/site/profile/manifests/puppetdb/app.pp
+++ puppet/code/environments/production/site/profile/manifests/puppetdb/app.pp
@@ -1,52 +1,41 @@
 class profile::puppetdb::app {
 
   $puppetdb_version = hiera('profiles::puppetdb::version')
+
   $database_hostname = hiera('profiles::puppetdb::database_hostname')
-  $total_nodes = hiera('profiles::puppetdb::total_nodes', 10)
+  $database_username = hiera('profiles::puppetdb::pgconf::user')
+  $database_password = hiera('profiles::puppetdb::pgconf::pass')
+
   $node_ttl = hiera('profiles::puppetdb::node_ttl', '90d')
   $report_ttl = hiera('profiles::puppetdb::report_ttl', '365d')
   $store_usage = hiera('profiles::puppetdb::store_usage', 10000)
   $temp_usage = hiera('profiles::puppetdb::temp_usage', 5000)
   $statements_cache_size = hiera('profiles::puppetdb::statements_cache_size', 0)
+  $java_heap_mem = hiera('profiles::puppetdb::java_heap_mem','256')
 
-  validate_integer($total_nodes)
   validate_integer($statements_cache_size)
 
-  #class {'puppetdb::globals':
-  #  version => $puppetdb_version,
-  #}
-
-  # 1024MB plus 2MB per node
-  $memory_jvm = 1024 + (2 * $total_nodes)
-
-  $memory_reserved = floor($::memorysize_mb * 0.2)
-
-  if $memory_reserved < 1024 {
-    $memory_limit = floor($::memorysize_mb - $memory_reserved)
-  } else {
-    $memory_limit = $::memorysize_mb - 1024
-  }
-
-  #if $memory_jvm > $memory_limit {
-  #  fail("Not enough memory for PuppetDB. Needs ${memory_jvm}MB and ${memory_limit}MB is available")
-  #}
-
-  #$java_args = { '-Xmx' => "${memory_jvm}m", '-Xms' => "${memory_jvm}m" }
-  $java_args = { '-Xmx' => "512m", '-Xms' => "512m" }
+  $java_args = { '-Xmx' => "${java_heap_mem}m", '-Xms' => "${java_heap_mem}m" }
 
   $command_threads = floor($::processorcount * 1.5)
 
-  class { 'puppetdb::server':
-    database_host          => $database_hostname,
-    manage_firewall        => false,
-    java_args              => $java_args,
-    listen_address         => '0.0.0.0',
-    command_threads        => $command_threads,
-    node_ttl               => $node_ttl,
-    report_ttl             => $report_ttl,
-    store_usage            => $store_usage,
-    temp_usage             => $temp_usage,
-    database_validate      => false,
+  class { '::puppetdb::globals':
+      version => $puppetdb_version,
+  }
+
+  class { '::puppetdb::server':
+    database_host     => $database_hostname,
+    database_username => $database_username,
+    database_password => $database_password,
+    manage_firewall   => false,
+    java_args         => $java_args,
+    listen_address    => '0.0.0.0',
+    command_threads   => $command_threads,
+    node_ttl          => $node_ttl,
+    report_ttl        => $report_ttl,
+    store_usage       => $store_usage,
+    temp_usage        => $temp_usage,
+    database_validate => false,
   }
 
   ini_setting { 'puppetdb_statements_cache_size':
