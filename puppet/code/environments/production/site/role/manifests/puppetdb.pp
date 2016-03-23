6cacb2cc525571e301af5a65d6df21e3a3255344
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-03-22 22:39:58 -0300

    Ativando profile puppet explorer por default

--- puppet/code/environments/production/site/role/manifests/puppetdb.pp
+++ puppet/code/environments/production/site/role/manifests/puppetdb.pp
@@ -4,9 +4,6 @@ class role::puppetdb {
   include profile::mcollective::server
   include profile::puppetdb::database
   include profile::puppetdb::app
-
-  # caso deseje instalar o puppet explorer
-  # descomente a linha abaixo
-  # include profile::puppetdb::frontend
+  include profile::puppetdb::frontend
 
 }
