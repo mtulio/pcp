cfd7812df337886f153c6f9e503dc175df1b25d9
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-03-22 22:37:50 -0300

    Refatoracao para simplificar a classe

--- puppet/code/environments/production/site/profile/manifests/puppet/server.pp
+++ puppet/code/environments/production/site/profile/manifests/puppet/server.pp
@@ -9,29 +9,7 @@ class profile::puppet::server {
 
   $jruby_instances = $::processorcount * 2
 
-  # 512MB plus 512MB per jruby instance
-  $memory_jvm = 512 + (512 * $jruby_instances)
-
-  $memory_reserved = floor($::memorysize_mb * 0.2)
-
-  if $memory_reserved < 1024 {
-    $memory_limit = floor($::memorysize_mb - $memory_reserved)
-  } else {
-    $memory_limit = $::memorysize_mb - 1024
-  }
-
-  #if $memory_jvm > $memory_limit {
-  #  fail("Not enough memory for PuppetServer. Needs ${memory_jvm}MB and ${memory_limit}MB is available")
-  #}
-
-  #$server_java_opts = "-Xms${memory_jvm}m -Xmx${memory_jvm}m"
   $server_java_opts = "-Xms512m -Xmx512m"
-
-  # TODO: gerenciar portas corretamente
-  service {'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
 
   # Bug relacionado: https://tickets.puppetlabs.com/browse/SERVER-557
   file {'/etc/systemd/system/puppetserver.service.d':
